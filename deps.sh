ca253a2ff953069f129d1714391104338be1d4fc
Ben Hall <ben@benhall.me.uk>
2016-05-14 19:19:37 +0100

    Change order

--- deps.sh
+++ deps.sh
@@ -1,12 +1,12 @@
 docker pull benhall/iperf:latest
 docker pull ubuntu:xenial
-docker build -f docker-images/io-perf/Dockerfile docker-images/io-perf
 docker pull redis:2.8
 docker pull benhall/redis:novol
 docker pull benhall/oscon2016-pg:latest
 docker pull benhall/elasticsearch:1.4.2
 docker pull benhall/strace:latest
 docker pull benhall/strace-ubuntu:latest
+docker build -f docker-images/io-perf/Dockerfile docker-images/io-perf
 
 # Optional (it's large download so depends on wifi)
 # docker pull benhall/metasploit:latest
