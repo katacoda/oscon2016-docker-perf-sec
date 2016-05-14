5e5192ff890358e7bb5e247f3fafdfd0c4768c48
Ben Hall <ben@benhall.me.uk>
2016-05-14 13:11:58 +0100

    strace, iperf osx client

--- deps.sh
+++ deps.sh
@@ -5,3 +5,5 @@ docker pull benhall/redis:novol
 docker pull benhall/oscon2016-pg:latest
 docker pull benhall/metasploit:latest
 docker pull benhall/elasticsearch:1.4.2
+docker pull benhall/strace:latest
+docker pull benhall/strace-ubuntu:latest
