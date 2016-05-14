1fbd99ffee38528cee263994a5b41893a8abf2e0
Ben Hall <ben@benhall.me.uk>
2016-05-14 21:50:37 +0100

    Updates

--- deps.sh
+++ deps.sh
@@ -1,7 +1,6 @@
 docker pull benhall/iperf:latest
 docker pull ubuntu:xenial
 docker pull redis:2.8
-docker pull benhall/redis:novol
 docker pull benhall/oscon2016-pg:latest
 docker pull benhall/elasticsearch:1.4.2
 docker pull benhall/strace:latest
@@ -10,3 +9,4 @@ docker build -f docker-images/io-perf/Dockerfile docker-images/io-perf
 
 # Optional (it's large download so depends on wifi)
 # docker pull benhall/metasploit:latest
+# docker pull benhall/redis:novol
