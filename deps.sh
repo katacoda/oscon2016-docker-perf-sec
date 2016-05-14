c0b9af6cfefd987072f762759e26d42a0533ed26
Ben Hall <ben@benhall.me.uk>
2016-05-14 19:10:19 +0100

    Optimise base images

--- deps.sh
+++ deps.sh
@@ -1,9 +1,12 @@
 docker pull benhall/iperf:latest
-docker pull benhall/ioperf-example:latest # build locally?
+docker pull ubuntu:xenial
+docker build -f docker-images/io-perf/Dockerfile docker-images/io-perf
 docker pull redis:2.8
 docker pull benhall/redis:novol
 docker pull benhall/oscon2016-pg:latest
-docker pull benhall/metasploit:latest
 docker pull benhall/elasticsearch:1.4.2
 docker pull benhall/strace:latest
 docker pull benhall/strace-ubuntu:latest
+
+# Optional (it's large download so depends on wifi)
+# docker pull benhall/metasploit:latest
