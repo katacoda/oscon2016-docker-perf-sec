a55de72cd2e9e6e232b2255c6cb8a96cccd56d66
Ben Hall <ben@benhall.me.uk>
2016-05-16 13:02:02 -0500

    More changes

--- images-required.sh
+++ images-required.sh
@@ -1,10 +1,14 @@
 docker pull benhall/iperf:latest
 docker pull ubuntu:xenial
-docker pull redis:2.8
+docker pull redis:3.2-alpine
 docker pull benhall/oscon2016-pg:latest
 docker pull benhall/elasticsearch:1.4.2
 docker pull benhall/strace:latest
 docker pull benhall/strace-ubuntu:latest
+docker pull nginx:1.9
+docker pull dockito/vault
+docker pull consul:v0.6.4
+docker pull cgswong/vault:latest
 docker build -f docker-images/io-perf/Dockerfile docker-images/io-perf
 
 # Optional (it's large download so depends on wifi)
